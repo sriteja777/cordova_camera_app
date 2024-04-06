e7a28567dce00b8b006bc6b9aa215d94c8aaa82f
sriteja777 <sritejakittu777@gmail.com>
2024-04-06 16:24:05 +0530

    added files

---
+++ www/js/index.js
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+
+
+let app = {
+    init: function() {
+        document.getElementById('btn').addEventListener('click', app.takePhoto);
+    },
+    takePhoto: function() { 
+        let opts = {
+            quality: 80,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            mediaType: Camera.MediaType.PICTURE,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 400
+        }
+        navigator.camera.getPicture(app.success, app.failure, opts);
+    },
+    success: function(imgURI) {
+        document.getElementById('photo').src = imgURI;
+        document.getElementById('msg').textContent = imgURI; 
+    },
+    failure: function (msg) {
+        document.getElementById('msg').textContent = msg;
+    }
+}
+
+document.addEventListener('deviceready', app.init, false);
+
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
